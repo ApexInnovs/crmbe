b972eba6caa656e72a32be1176a032553f1e02e1
jyotiranjan862 <dhaljyotiranjan995@gmail.com>
2026-04-15 00:08:35 +0530

    a

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors('*'));
 
 // Rate Limiter
 // const limiter = rateLimit({
